a3a26fcf557be89f6a6891842ce5f3080af24453
Suanya <sonjaunger@eyewant.me>
2024-04-28 13:42:28 +0200

    wip

--- _BettyMü/Assets/ShowRoom/Scripts/BubbleFloater.cs
+++ _BettyMü/Assets/ShowRoom/Scripts/BubbleFloater.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Random = UnityEngine.Random;
 
 public class BubbleFloater : MonoBehaviour
 {
     public float floatSpeed = 1f; // Speed at which the object floats
+    public float windForce = 0.5f;
+
+    private void Start()
+    {
+        ApplyWindForce();
+    }
 
     void Update()
     {
         // Move the object upwards
         transform.Translate(Vector3.up * floatSpeed * Time.deltaTime);
+    }
+
+    private void ApplyWindForce()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere;
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null ) 
+        {
+            rb.AddForce(randomDirection * windForce, ForceMode.Impulse);
+        }
     }
 }
 
