79043762ebb27ac4404ac69ed5c33809aa61b21d
Suanya <sonjaunger@eyewant.me>
2024-05-09 21:42:17 +0200

    visualStudio check

---
+++ _BettyMü/Assets/MultiImage/Scripts/MultiTracker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+
+
+public class MultiTracker : MonoBehaviour
+{
+    [SerializeField] private GameObject[] arObjectsToPlace;
+
+    private ARTrackedImageManager m_TrackeImageManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
